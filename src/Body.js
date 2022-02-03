33069dab1f83d2e295c249cad559f88d7a33f99e
Mideolabanji <olabanjikaosara@yahoo.com>
2022-02-03 15:28:14 +0100

    included a component for the weather data

---
+++ src/Body.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Body(props) {
+  return (
+    <div className="Body">
+      <h2>{props.property.cityName}</h2>
+      <h6>Last updated: Sunday 11:52</h6>
+      <h5 className="weather-description">{props.property.description}</h5>
+      <div className="row">
+        <div className="col-6">
+          <h1>
+            <span className="city-temperature">
+              {Math.round(props.property.temperature)}
+            </span>
+            <span className="degree">C</span>
+          </h1>
+        </div>
+        <div className="col-6">
+          <img src={props.property.icon} alt={props.property.description} />
+        </div>
+      </div>
+      <ul>
+        <li>Humidity: {props.property.humidity}%</li>
+        <li>Wind: {props.property.wind}Km/h</li>
+      </ul>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,70 +1,56 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import Body from "./Body";
 
 export default function Weather() {
-  let [body, setBody] = useState({});
-  let [city, setCity] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
+  const [loaded, setLoaded] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
-
-    function showTemperature(response) {
-      setBody({
-        cityName = response.data.name,
-        description = response.data.weather[0].description,
-        temperature = Math.round(response.data.main.temp),
-        icon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-        humidity = response.data.main.humidity,
-        wind = Math.round(response.data.wind.speed)
-      });
-    }
-
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=872e937052bfbc7cf66f2ac8c3fcaba7&units=metric`;
-    axios.get(url).then(showTemperature);
   }
 
-  function updateCity(event) {
-    setCity(event.target.value);
+  function displayWeatherData(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      cityName: response.data.name,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+    });
+    setLoaded(true);
   }
 
-  return (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              onChange={updateCity}
-              className="form-control"
-              type="search"
-              placeholder="Enter a city"
-              autoFocus="on"
-            />
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Enter a city"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3 ">
+              <input
+                className="btn btn-secondary"
+                type="submit"
+                value="Search"
+              />
+            </div>
           </div>
-          <div className="col-3 ">
-            <input className="btn btn-secondary" type="submit" value="Search" />
-          </div>
-        </div>
-      </form>
-      <h2>{body.cityName}</h2>
-      <h6>Last updated: Sunday 11:51</h6>
-      <h5 className="weather-description">{body.description}</h5>
-      <div className="row">
-        <div className="col-6">
-          <h1>
-            <span className="city-temperature">
-              {body.temperature}
-            </span>
-            <span className="degree">°C</span>
-          </h1>
-        </div>
-        <div className="col-6">
-          <img src={body.icon} alt={body.description} />
-        </div>
+        </form>
+        <Body property={weatherData} />
       </div>
-      <ul>
-        <li>Humidity: {body.humidity}%</li>
-        <li>Wind: {body.wind}km/h</li>
-      </ul>
-    </div>
-  );
+    );
+  } else {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=872e937052bfbc7cf66f2ac8c3fcaba7&units=metric`;
+    axios.get(apiUrl).then(displayWeatherData);
+    return "Loading...";
+  }
 }
